f5205231b90f7d2a7085e6c2378c2db444c35bb2
mirzapurnandi <mirza.purnandi@gmail.com>
2025-09-28 11:57:27 +0700

    Add Dist 2

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,40 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/* export default defineConfig({
   plugins: [vue()],
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+}) */
+
+export default defineConfig({
+    plugins: [vue()],
+
+    // base URL → gunakan '/' karena langsung di root subdomain
+    base: "/",
+
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+
+    build: {
+        outDir: "dist", // folder hasil build
+        assetsDir: "assets", // folder untuk JS/CSS/images
+        sourcemap: false, // biar file build lebih kecil
+        target: "es2015", // lebih compatible di browser lama
+    },
+
+    /* server: {
+        port: 5173, // hanya untuk local dev
+        open: true,
+    }, */
+});
